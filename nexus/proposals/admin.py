5ce36d35812a3fe4647be84f230d0ae975dbc549
storymode7 <17mayanksinghal@gmail.com>
2018-09-26 02:05:11 +0530

    feat(proposals/admin): Add admin.py for proposals

---
+++ nexus/proposals/admin.py
@@ -0,0 +1,18 @@
+# Third Party Stuff
+from django.contrib import admin
+
+# nexus Stuff
+from nexus.proposals.models import Proposal
+
+
+@admin.register(Proposal)
+class ProposalAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Proposal info', {'fields': ('title', 'speaker', 'status', 'kind', 'level',
+                                      'duration', 'abstract', 'description')}),
+        ('Timing', {'fields': ('approved_at',)}),
+    )
+    list_display = ('speaker', 'title', 'submitted_at', 'status')  # shows UUID for speaker
+    list_filter = ('speaker', 'submitted_at', 'status')
+    ordering = ('submitted_at',)
+    search_fields = ('speaker__email', 'speaker__first_name', 'speaker__last_name')
