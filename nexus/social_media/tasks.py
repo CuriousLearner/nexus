61ba584ca9bd504f8c5277202399d91767d11ded
GeekyShacklebolt <shivasaxena911@gmail.com>
2019-03-02 20:02:55 +0530

    fix(tasks): Remove unnecessary BaseTask class

--- nexus/social_media/tasks.py
+++ nexus/social_media/tasks.py
@@ -1,18 +1,11 @@
 # Third Party Stuff
-import celery
 from celery.decorators import task
 
 # nexus Stuff
 from nexus.social_media import services
 
 
-class BaseTask(celery.Task):
-
-    def on_failure(self, exc, task_id, args, kwargs, einfo):
-        print('{0!r} failed: {1!r}'.format(task_id, exc))
-
-
-@task(base=BaseTask)
+@task()
 def task_to_post_to_twitter(post_id):
     """Celery task to post on twitter.
 
