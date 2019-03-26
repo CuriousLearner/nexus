3034add84799aae104d71c95814a903f5e763fd3
GeekyShacklebolt <shivasaxena911@gmail.com>
2019-03-26 15:02:09 +0530

    fix(proposal): Minor naming changes in models test

--- nexus/proposals/tests/test_models.py
+++ nexus/proposals/tests/test_models.py
@@ -6,17 +6,17 @@ from tests import factories as f
 from nexus.proposals.models import Proposal, ProposalKind
 
 
-class TestProposalModel(TestCase):
+class ProposalModelTestCase(TestCase):
 
     def setUp(self):
         self.user = f.create_user(email='test@example.com', password='test')
-        self.proposalkind = f.create_proposal_kind(kind='talk')
+        self.proposal_kind = f.create_proposal_kind(kind='talk')
 
     def test_create_proposal(self):
         proposal = {
             'speaker': self.user,
             'title': 'Test Proposal',
-            'kind': self.proposalkind,
+            'kind': self.proposal_kind,
             'level': 'beginner',
             'duration': '00:30:00',
             'abstract': 'Proposal abstract',
@@ -26,7 +26,7 @@ class TestProposalModel(TestCase):
         assert proposal.id
         assert proposal.speaker == self.user
         assert proposal.title == 'Test Proposal'
-        assert proposal.kind == self.proposalkind
+        assert proposal.kind == self.proposal_kind
         assert proposal.level == Proposal.LEVELS_CHOICES.BEGINNER
         assert proposal.duration == '00:30:00'
         assert proposal.abstract == 'Proposal abstract'
@@ -37,7 +37,7 @@ class TestProposalModel(TestCase):
         assert str(proposal) == str(proposal.title)
 
 
-class TestProposalKindModel(TestCase):
-    def test_create_proposalkind(self):
-        proposalkind = ProposalKind.objects.create(kind='talk')
-        assert str(proposalkind) == str(proposalkind.kind)
+class ProposalKindModelTestCase(TestCase):
+    def test_create_proposal_kind(self):
+        proposal_kind = ProposalKind.objects.create(kind='talk')
+        assert str(proposal_kind) == str(proposal_kind.kind)
